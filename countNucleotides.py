84a42b6bbb812cb96d5cd294ebda76978e1b4d98
evoxtorm <evostorm96@gmail.com>
2019-04-08 23:01:25 +0530

    Count DNA Nucleotides

--- countNucleotides.py
+++ countNucleotides.py
@@ -3,5 +3,18 @@
 
 someString = 'AGCTTTTCATTCTGACTGCAACGGGCAATATGTCTCTGTGTGGATTAAAAAAAGAGTGTCTGATAGCAGC'
 
+aCount = 0
+cCount = 0
+gCount = 0
+tCount = 0
+
 for i in someString:
-    print(i)
+    if i == 'A':
+        aCount += 1
+    elif i == 'C':
+        cCount += 1
+    elif i == 'G':
+        gCount += 1
+    else:
+        tCount += 1
+print(aCount, cCount, gCount, tCount)
